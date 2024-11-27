ad2ca3da50e90bfef61e9150922c39c74eda61e3
Rafael Moura <rafael.mesquita.moura@gmail.com>
2024-11-27 20:03:23 -0300

    fix: Adjust pipeline

--- versions.tf
+++ versions.tf
@@ -12,6 +12,10 @@ terraform {
   }
 }
 
+provider "aws" {
+  # Configuration options
+}
+
 provider "mongodbatlas" {
   public_key    = "xnkugwgf"
   private_key   = "7d5a730b-cb5f-4496-8fb5-5ffaf7d5a0f3"
