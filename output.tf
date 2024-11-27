5ae664c3486b39f3b378760aeacb2add1f369bdf
Rafael Moura <rafael.mesquita.moura@gmail.com>
2024-11-27 20:59:11 -0300

    fix: Adjust pipeline

--- output.tf
+++ output.tf
@@ -20,25 +20,25 @@ output "rds_product" {
 
 output "ecr_payments" {
     description = "ECR Payments"
-    value       = aws_ecr_repository.payments.connection
+    value       = aws_ecr_repository.payments.repository_url
 }
 
 output "ecr_products" {
     description = "ECR Products"
-    value       = aws_ecr_repository.products.connection
+    value       = aws_ecr_repository.products.repository_url
 }
 
 output "ecr_productions" {
     description = "ECR Productions"
-    value       = aws_ecr_repository.productions.connection
+    value       = aws_ecr_repository.productions.repository_url
 }
 
 output "ecr_orders" {
     description = "ECR Orders"
-    value       = aws_ecr_repository.orders.connection
+    value       = aws_ecr_repository.orders.repository_url
 }
 
 output "ecr_customers" {
     description = "ECR Customers"
-    value       = aws_ecr_repository.customers.connection
+    value       = aws_ecr_repository.customers.repository_url
 }
